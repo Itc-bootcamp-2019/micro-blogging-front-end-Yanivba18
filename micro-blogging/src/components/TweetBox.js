65e87ebe6de8430b86e05fa2851577d7caf653a4
Yanivba18 <47143949+Yanivba18@users.noreply.github.com>
2019-12-16 17:48:01 +0200

    Added field validation

--- micro-blogging/src/components/TweetBox.js
+++ micro-blogging/src/components/TweetBox.js
@@ -5,11 +5,27 @@ class TweetBox extends React.Component {
         super(props);
         this.state = {
             input: "",
+            inputValid: true,
         }
     }
 
     onInputChange(e) {
-        this.setState({ input: e.target.value });
+        const value = e.target.value.length;
+        console.log(value)
+        this.setState({ input: e.target.value },
+            () => { this.validateField(value) });
+    }
+
+    validateField(value) {
+        if (value > 140) {
+            this.setState( {inputValid: false} )
+        } else {
+            this.setState( { inputValid: true} )
+        }
+    }
+
+    btnOnClick() {
+        alert("Not implemented yet")
     }
 
     render() {
@@ -18,7 +34,7 @@ class TweetBox extends React.Component {
             <div className="tweet-box">
                 <textarea className="tweet-box-input" value={this.state.input} onChange={(e) => { this.onInputChange(e) }} placeholder="What's on your mind..." >
                 </textarea>
-
+                <button type="button" disabled={!this.state.inputValid} className="btn" onClick={() => {this.btnOnClick()}}>Tweet</button>
             </div>
             
         )
