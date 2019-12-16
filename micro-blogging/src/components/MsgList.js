d392363912c214875dafe15107cddba59752c7b6
Yanivba18 <47143949+Yanivba18@users.noreply.github.com>
2019-12-16 21:10:23 +0200

    Created MsgList component

---
+++ micro-blogging/src/components/MsgList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class MsgList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            messages: [],
+        }
+    }
+
+    render() {
+        return (
+            
+        )
+    }
+}
