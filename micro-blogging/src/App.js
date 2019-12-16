f756b7586ea2a4b9a56ccd79e4b3bab166e43f28
Yanivba18 <47143949+Yanivba18@users.noreply.github.com>
2019-12-16 23:59:36 +0200

    Added msgList component

--- micro-blogging/src/App.js
+++ micro-blogging/src/App.js
@@ -2,13 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TweetBox from './components/TweetBox';
+import MsgList from './components/MsgList';
 
-function App() {
-  return (
-    <div className="App">
-      <TweetBox />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currMsg: '',
+    }
+  }
+
+  render(){
+
+    return (
+      <div className="App">
+        <TweetBox />
+        <MsgList />
+      </div>
+    );
+  }
 }
 
 export default App;
