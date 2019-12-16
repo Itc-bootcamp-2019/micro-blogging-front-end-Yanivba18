1b4da448890b398d34521b323da38891b0af0b0e
Yanivba18 <47143949+Yanivba18@users.noreply.github.com>
2019-12-17 00:00:57 +0200

    MsgList now shows a list of tweets from api

--- micro-blogging/src/components/MsgList.js
+++ micro-blogging/src/components/MsgList.js
@@ -1,16 +1,40 @@
 import React from 'react';
+import { getAllTweets } from "./api"
+import Msg from './Msg';
 
 class MsgList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             messages: [],
+            loading: true,
         }
     }
 
+    componentDidMount() {
+        getAllTweets().then((resolve) => {
+            console.log(resolve)
+            this.setState({
+                messages: resolve.data.tweets,
+                loading: false
+            })
+        })
+    }
+
     render() {
+        const { messages } = this.state;
         return (
-            
+            <div>
+                {this.state.loading && <span className="msg-box loading">Loading...</span>}
+                
+                {!this.state.loading && messages.map(msgObj => {
+                    return (
+                        <Msg key={msgObj.date} user={msgObj.userName} date={msgObj.date} msg={msgObj.content}/>
+                    )
+                })}
+            </div>
         )
     }
 }
+
+export default MsgList;
