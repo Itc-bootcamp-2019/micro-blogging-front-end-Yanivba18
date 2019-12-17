4366ad1a71464128cd52289339b7f6f6b295d454
Yanivba18 <47143949+Yanivba18@users.noreply.github.com>
2019-12-17 22:00:41 +0200

    Added profile page, navbar and routing

--- micro-blogging/src/App.js
+++ micro-blogging/src/App.js
@@ -1,29 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route, BrowserRouter as Router, Link } from 'react-router-dom';
 import './App.css';
 import TweetBox from './components/TweetBox';
 import MsgList from './components/MsgList';
 import NavBar from './components/Navbar';
+import Profile from './components/Profile';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currMsg: '',
+      currTweet: undefined,
     }
+  }
+
+  getTweetCallback = (tweet) => {
+    this.setState({ currTweet: tweet }, () => {console.log(this.state)})
   }
 
   render() {
 
     return (
-      <>
+      <Router>
         <NavBar />
         <div className="App">
-
-          <TweetBox />
-          <MsgList />
+          <Switch>
+            <Route exact path="/">
+              <TweetBox callback={this.getTweetCallback}/>
+              <MsgList newTweet={this.state.currTweet}/>
+            </Route>
+            <Route exact path="/profile/">
+              <Profile />
+            </Route>
+          </Switch>
         </div>
-      </>
+      </Router>
     );
   }
 }
