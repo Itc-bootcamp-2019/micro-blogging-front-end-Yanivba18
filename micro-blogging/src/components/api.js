90dde1d847f31dbb4743badd3fa0cba4a4dc8f80
Yanivba18 <47143949+Yanivba18@users.noreply.github.com>
2019-12-17 22:01:02 +0200

    Changed createTweet to receive an object argument

--- micro-blogging/src/components/api.js
+++ micro-blogging/src/components/api.js
@@ -6,18 +6,11 @@ export function getAllTweets() {
     return axios.get(`https://itc-bootcamp-19-dot-charcha-dev.appspot.com/tweet`);
 }
 
-export function createTweet(content, userName) {
-    const date = new Date();
-    const today = date.toISOString();
-    const payload = {
-        content: content,
-        userName: userName,
-        date: today
-    }
+export function createTweet(tweet) {        
     return axios({
         method: 'post',
         url: `https://itc-bootcamp-19-dot-charcha-dev.appspot.com/tweet`, 
-        data: { tweet: payload }
+        data: { tweet: tweet }
     });
 }
 
